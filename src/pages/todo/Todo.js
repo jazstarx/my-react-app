e4aac968003ba1d3fbdaa0183c53cb30f1966c48
jasakran.singh <jaskaran.singh@mercurygate.com>
2024-03-13 11:59:47 +0530

    css fixes

--- src/pages/todo/Todo.js
+++ src/pages/todo/Todo.js
@@ -49,8 +49,22 @@ function Todo() {
           toggleCompleted={toggleCompleted}
         />
       ))}
+
+      <button onClick={() => addTask(text)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="15px"
+          height="15px"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M11.25 12.75V18H12.75V12.75H18V11.25H12.75V6H11.25V11.25H6V12.75H11.25Z"
+            fill="#080341"
+          />
+        </svg>
+      </button>
       <input value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={() => addTask(text)}>Add</button>
     </div>
   );
 }
--- src/pages/todo/TodoItem.js
+++ src/pages/todo/TodoItem.js
@@ -8,8 +8,23 @@ function TodoItem({ task, deleteTask, toggleCompleted }) {
   return (
     <div className="todo-item">
       {/* <input type="checkbox" checked={task.completed} onChange={handleChange} /> */}
-      <p>{task.text}</p>
-      <button onClick={() => deleteTask(task.id)}>X</button>
+      <button onClick={() => deleteTask(task.id)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="15px"
+          height="15px"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M9 4.5V6H6V7.5H18V6H15V4.5H9ZM6.75 8.25H8.25V17.6893L8.56066 18H15.4393L15.75 17.6893V8.25H17.25V18.3107L16.0607 19.5H7.93934L6.75 18.3107V8.25Z"
+            fill="#080341"
+          />
+        </svg>
+      </button>
+      <span>{task.text}</span>
     </div>
   );
 }
